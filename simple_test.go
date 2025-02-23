2fcdc750b2c9a4e4fb80d0aff7156e8a631f6487
Willams Sousa <osdeving@gmail.com>
2025-02-23 17:04:34 -0300

    exemplos de subtests com t.Run t.Skipped e t.Failed

--- simple_test.go
+++ simple_test.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 )
 
+type SumTest struct {
+	testValues []int
+	expectedResult int
+}
+
 func TestSum(t *testing.T) {
-	testValues := []int {10, 20, 30}
-	_, sum := sortAndTotal(testValues)
+	sumTests := []SumTest {
+		{testValues: []int{10, 20, 30}, expectedResult: 10},
+		{testValues: []int{-10, 0, -10}, expectedResult: -20},
+		{testValues: []int{-10, 0, -10}, expectedResult: -20},
+	}
 
-	expected := 60
+	for index, sumTest := range sumTests {
+		t.Run(fmt.Sprintf("Sum #%d", index), func(subT *testing.T) {
+			_, total := sortAndTotal(sumTest.testValues)
 
-	if sum != expected {
-		t.Errorf("Expected %d, Got %d\n", expected, sum)
+			if t.Failed() {
+				subT.SkipNow()
+			}
+
+			if total != sumTest.expectedResult {
+				subT.Fatalf("Expected %d, Got %d\n", sumTest.expectedResult, total)
+			}
+		})
+
 	}
 }
 
@@ -22,5 +40,33 @@ func TestSort(t *testing.T) {
 
 	if !sort.IntsAreSorted(sorted) {
 		t.Errorf("List is not sorted")
+	}
+}
+
+func TestSort2(t *testing.T) {
+	slices := [][]int {
+		{1, 2, 23, 14, 5, 6},
+		{4, 5, 6, 8, 10, 11},
+		{7, 18, 9, 3, 12, 1},
+		{10, 11, 12, 13, 14, 15},
+		{1, 2, 3, 4, 5, 6},
+	}
+
+	for index, slice := range slices {
+		t.Run(fmt.Sprintf("Sort #%d", index), func(subT *testing.T) {
+			if index == 1 {
+				subT.Skip("Skipping test")
+			}
+
+			sorted, _ := sortAndTotal(slice)
+
+			if index == 2 {
+				subT.FailNow()
+			}
+
+			if !sort.IntsAreSorted(sorted) {
+				subT.Errorf("Unsorted slice: %d\n", slice)
+			}
+		})
 	}
 }
