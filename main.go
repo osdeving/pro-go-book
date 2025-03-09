f2cffe234618d4b229b017b576992cd9ed6ae3dd
Willams Sousa <osdeving@gmail.com>
2025-03-08 21:03:42 -0300

    demonstrar thread principal saindo antes das sub rotinas terminarem

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+
+func doSum(count int, val *int) {
+	for i := 0; i < count; i++ {
+		*val++
+	}
+}
+
+func main() {
+	counter := 0
+	go doSum(5000, &counter)
+	Printfln("Total: %v", counter)
+}
+
